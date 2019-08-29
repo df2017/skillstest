20c28fb0de45f0dbc19a50c6ef7b8c5fec195820
Diego <dferrey22783@gmail.com>
2019-08-28 23:16:06 -0300

    upgrade

--- userskills/wsgi.py
+++ userskills/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skillstest.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'userskills.settings')
 
 application = get_wsgi_application()
